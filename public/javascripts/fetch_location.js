b3f4f7644ccba6cbba50cb683acec46ea1b99f5c
sim1hill <simone.c.hill@gmail.com>
2015-05-18 09:48:35 -0400

    done-ish

--- public/javascripts/fetch_location.js
+++ public/javascripts/fetch_location.js
@@ -27,42 +27,29 @@ $(document).ready(function() {
   var location = parsed_json['location']['city'];
   var temp_f = parsed_json['current_observation']['temp_f'];
   printTemp(location,temp_f)
+  decideJacket(temp_f)
   }
   });
   }
 
   function printTemp(location,temp){
-    $('#test h1').replaceWith("<h1>It's currently " + temp +" degrees in " + location);
+    $('#current-weather').append("<h1>It's currently " + temp +" degrees in " + location);
+  }
+
+  function decideJacket(temp_f){
+    if ((temp_f >= 60) && (temp_f <= 72)){
+      $('#jacket').prepend("¯/\/_(ツ)_/¯...bring something light along just in case<br><img src='hoodie.gif'></img>")
+    } else if((temp_f > 32) && (temp_f <= 55)){
+      $('#jacket').prepend("Definitely, pack a jacket or layer up.<br><img src='coat.gif'></img>")
+
+    } else if (temp_f < 32){
+      $('#jacket').prepend("You do you, but if you freeze to death that's not on me.<br><img src='olaf.gif'></img>")
+    }else if(temp_f > 73){
+      $('#jacket').prepend("Leave the jacket at home or things are gonna get toasty.<br><img src='hot.gif'></img>")
+    }
   }
 
 });
-
-
-// $(document).ready(function(){
-//   var geocoder = new google.maps.Geocoder();
-//   function getLocation() {
-//     // code here
-//     var location = navigator.geolocation.getCurrentPosition(function(position){
-//       var longi = position.coords.longitude
-//       var lat = position.coords.latitude
-//       codeLatLng(lat,longi);
-      
-//     });
-//   }
-//   getLocation();
-
-
-//  function codeLatLng(lat,lng) {
-//   debugger;
-//   var latlng = new google.maps.LatLng(lat, lng);
-// //   geocoder.geocode({'latLng': latlng}, function(results, status) {
-// //     debugger;
-// //     }
-// }
-
-// // google.maps.event.addDomListener(window, 'load', initialize);
-
-// });
 
 
 
---
+++ public/javascripts/form_logic.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  var date = Date.now();
+  $('label#date_1').replaceWith('<label for="date_1">' + date + '</label>')
+});
+
+function decideOnJacket(datesArray){
+  // if datesArray[0]
+}
